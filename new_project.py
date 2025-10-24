c747692b5a27f02196b7820f011901705a06f5ba
Jung Hyun kim <nase1501@naver.com>
2025-10-24 20:41:55 +0900

    first commit

---
+++ new_project.py
@@ -0,0 +1,36 @@
+# 객체 지향 프로그래밍 (OOP): Object-Oriented Programming
+
+# car.speed , Object.Attribute
+# car.stop(), Object.method (객체묶여버린 함수)
+#
+# import another_module
+#
+# print(another_module.another_variable)
+
+# from turtle import Turtle, Screen
+# timmy =Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkBlue")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+
+table.field_names =["Pokemon Name", "Type"]
+table.add_rows([
+    ["Pikachu","Electric"],
+    ["Squirtle","Water"],
+    ["Charmander","Fire"]
+])
+
+# table.add_column("Pokemon Name",[])
+# table.add_column("Type,[])
+
+table.align = "l"
+
+print(table)
